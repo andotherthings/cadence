df2deafa5e527bad2bd5c1ff3b9999a0ab73054f
mtcrouse <mettacrouse@gmail.com>
2017-02-03 14:23:19 -0800

    fix linting problems

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,14 @@
-'use strict';
-
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/cadence_dev'
+    connection: 'postgres://localhost/cadence_dev',
   },
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/cadence_test'
+    connection: 'postgres://localhost/cadence_test',
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
+    connection: process.env.DATABASE_URL,
+  },
 };
