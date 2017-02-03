a19329a47a26dc602b30e8f6fd2c7ee48ea11a9b
mtcrouse <mettacrouse@gmail.com>
2017-02-03 10:59:35 -0800

    finish migrations

---
+++ migrations/20170203105127_cards_decks.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cards_decks', (table) => {
+    table.increments();
+    table.integer('deck_id')
+      .notNullable()
+      .references('id')
+      .inTable('decks')
+      .onDelete('CASCADE')
+      .index();
+    table.integer('card_id')
+      .notNullable()
+      .references('id')
+      .inTable('cards')
+      .onDelete('CASCADE')
+      .index();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('cards_decks');
+};
