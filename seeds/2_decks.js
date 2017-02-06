f1a7c64347afe149e05058ce94a2ac0911bc9fda
smk291 <s.marius.kurland@gmail.com>
2017-02-06 01:30:41 -0800

    Seeds linted. Remaining warnings are irrelevant, remaining errors incorrect.

--- seeds/2_decks.js
+++ seeds/2_decks.js
@@ -1,29 +1,28 @@
 /* eslint-disable max-len, camelcase */
-'use strict';
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex('decks').del()
     .then(() => {
       return knex('decks').insert([{
         id: 1,
-        topic: "Promises & Asynchronous Programming",
+        topic: 'Promises & Asynchronous Programming',
         created_at: new Date('2016-06-26 14:26:16 UTC'),
-        updated_at: new Date('2016-06-26 14:26:16 UTC')
+        updated_at: new Date('2016-06-26 14:26:16 UTC'),
       }, {
         id: 2,
-        topic: "Fundamentals",
+        topic: 'Fundamentals',
         created_at: new Date('2016-06-26 14:26:16 UTC'),
-        updated_at: new Date('2016-06-26 14:26:16 UTC')
+        updated_at: new Date('2016-06-26 14:26:16 UTC'),
       }, {
         id: 3,
-        topic: "HTTP",
+        topic: 'HTTP',
         created_at: new Date('2016-06-26 14:26:16 UTC'),
-        updated_at: new Date('2016-06-26 14:26:16 UTC')
+        updated_at: new Date('2016-06-26 14:26:16 UTC'),
       }]);
     })
     .then(() => {
       return knex.raw(
-        "SELECT setval('decks_id_seq', (SELECT MAX(id) FROM decks));"
+        'SELECT setval(\'decks_id_seq\', (SELECT MAX(id) FROM decks));'
       );
     });
 };
