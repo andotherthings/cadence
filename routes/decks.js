8aa5ed2be6935b40398321085a327aa0eadd39d8
mrjadaml <mrjadaml@gmail.com>
2017-02-07 12:37:31 -0800

    refactored routes fixes #19

--- routes/decks.js
+++ routes/decks.js
@@ -3,91 +3,89 @@ const knex = require('../knex');
 
 const router = express.Router();
 
-router.post('/decks', (req, res, next) => {
+router.get('/', (req, res, next) => {
+  knex('decks')
+    .then(decks => {
+      res.send(decks);
+    })
+    .catch(err => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('decks')
+    .where('id', id)
+    .first()
+    .then(deck => {
+      if (!deck) {
+        res.status(400).send('No deck found');
+      }
+
+      res.send(deck);
+    })
+    .catch(err => next(err));
+});
+
+router.post('/', (req, res, next) => {
   const { topic } = req.body;
 
   knex('decks')
-  .insert({ topic }, '*')
-  .then((newCard) => {
-    res.send(newCard);
-  })
-  .catch((err) => {
-    next(err);
-  });
-});
-
-router.get('/decks/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  knex('decks')
-  .where('id', id)
-  .first()
-  .then((row) => {
-    if (!row) {
-      res.status(400).send('No deck found');
-    }
-
-    res.send(row);
-  })
-  .catch((err) => {
-    next(err);
-  });
-});
-
-router.patch('/decks/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  knex('decks')
-  .where('id', id)
-  .first()
-  .then((row) => {
-    if (!row) {
-      res.status(400).send('No deck found');
-    }
-
-    const updateRow = {};
-
-    if (req.body.topic) {
-      updateRow.topic = req.body.topic;
-    }
-
-    knex('decks')
-    .where('id', id)
-    .update(updateRow, '*')
-    .then((updatedRow) => {
-      res.send(updatedRow);
+    .insert({ topic }, '*')
+    .then(newDeck => {
+      res.send(newDeck);
     })
-    .catch((err) => {
-      next(err);
-    });
-  })
-  .catch((err) => {
-    next(err);
-  });
+    .catch(err => next(err));
 });
 
-router.delete('/decks/:id', (req, res, next) => {
+router.patch('/:id', (req, res, next) => {
   const { id } = req.params;
 
   knex('decks')
-  .where('id', id)
-  .first()
-  .then((row) => {
-    if (!row) {
-      res.status(400).send('No deck found');
-    }
-
-    knex('decks')
     .where('id', id)
-    .del()
-    .returning('*')
-    .then((deletedRow) => {
-      res.send(deletedRow);
-    });
-  })
-  .catch((err) => {
-    next(err);
-  });
+    .first()
+    .then(deck => {
+      if (!deck) {
+        res.status(400).send('No deck found');
+      }
+
+      const updateDeck = {};
+
+      if (req.body.topic) {
+        updateDeck.topic = req.body.topic;
+      }
+
+      knex('decks')
+        .where('id', id)
+        .update(updateDeck, '*')
+        .then(updatedDeck => {
+          res.send(updatedDeck);
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex('decks')
+    .where('id', id)
+    .first()
+    .then(deck => {
+      if (!deck) {
+        res.status(400).send('No deck found');
+      }
+
+      knex('decks')
+        .where('id', id)
+        .del()
+        .returning('*')
+        .then(deletedDeck => {
+          res.send(deletedDeck);
+        });
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const path = require('path');
 const webpack = require('webpack');
 const config = require('./webpack.config');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8008;
 const compiler = webpack(config);
 
-const cards = require('./routes/cards');
-const decks = require('./routes/decks');
-const cardsDecks = require('./routes/cards_decks');
+const routes = require('./routes/index');
 
 app.use(bodyParser.json());
-app.use(cards);
-app.use(decks);
-app.use(cardsDecks);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath,
@@ -23,13 +17,10 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   /* eslint-disable no-console */
 
-  console.log('on some port');
+  console.log(`Application running at http://localhost:${port}`);
 });
