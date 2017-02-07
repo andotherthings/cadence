5d348fbcbcddaf2312ff6d06aa45a17891f6416b
smk291 <s.marius.kurland@gmail.com>
2017-02-06 18:46:29 -0800

    user can now retrieve deck

--- routes/cards_decks.js
+++ routes/cards_decks.js
@@ -12,9 +12,20 @@ router.post('/cards_decks', (req, res, next) => {
   .then((newCardDeck) => {
     res.send(newCardDeck);
   })
-  .catch((err) => {
-    next(err);
-  });
+  .catch((err) => next(err));
+});
+
+router.get('/cards_decks', (req, res, next) => {
+  const { deckId } = req.body;
+
+  knex('cards_decks')
+  .where('deck_id', deckId)
+  .join('decks', 'decks.id', 'deck_id')
+  .join('cards', 'cards.id', 'card_id')
+  .then((deck) => {
+    res.send(camelizeKeys(deck));
+  })
+  .catch((err) => next(err))
 });
 
 module.exports = router;
