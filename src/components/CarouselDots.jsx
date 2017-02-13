05c64a34ed42817d9f856647547eb465b5fd6247
smk291 <s.marius.kurland@gmail.com>
2017-02-13 13:46:58 -0500

    removed unecessary

--- src/components/CarouselDots.jsx
+++ src/components/CarouselDots.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export default class CarouselDots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dotSequence: '',
+    };
+  }
+
+  componentWillReceiveProps(nextProps){
+    let sequence = '';
+
+    for (let i = 0; i < this.props.dotCount; i++) {
+      i !== nextProps.currentDot ? sequence += '●' : sequence += '○';
+    }
+
+    this.setState({dotSequence: sequence})
+  }
+
   render () {
     return (
-      <div className="dots twelve columns"> ● ● ● ○ ● ● ● ● ● ● ● ● </div>
+      <div className="dots twelve columns"> {this.state.dotSequence} </div>
     );
   }
 }
