fb0317d148782826b6fdb1a63a8d80f15e0afccc
smk291 <s.marius.kurland@gmail.com>
2017-02-13 13:46:58 -0500

    cards now cycle, flip, show data

--- src/components/CarouselDots.jsx
+++ src/components/CarouselDots.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 export default class CarouselDots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dotSequence: '',
+    }
+    this.createDotSequence = this.createDotSequence.bind(this);
+  }
+
+  createDotSequence(propsSource){
+    let sequence = '';
+
+    for (let i = 0; i < this.props.dotCount; i++) {
+      i !== propsSource.currentDot ? sequence += '●' : sequence += '○';
+    }
+    
+    this.setState({dotSequence: sequence})
+  }
+
+  componentDidMount(nextProps, nextState){
+    this.createDotSequence(this.props);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.createDotSequence(nextProps);
+  }
+
   render () {
     return (
-      <div className="dots twelve columns"> ● ● ● ○ ● ● ● ● ● ● ● ● </div>
+      <div className="dots twelve columns"> {this.state.dotSequence} </div>
     );
   }
 }
